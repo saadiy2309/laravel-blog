9a8133fa3dfadc7c327b2cca88fa9ffda5918979
saadiy2309 <sadriddin2309@gmail.com>
2022-04-21 04:50:59 +0500

    first Commit to laravel blog

---
+++ resources/views/post.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+
+<title>My blog</title>
+<link rel="stylesheet" type="text/css" href="/app.css">
+
+
+<body>
+    <article>
+       <?= $post; ?>
+    </article>
+
+    <a href="/">Go Back</a>
+</body>
