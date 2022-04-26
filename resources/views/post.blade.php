7925652a19df3332b3daaf67ea4840094885d6fb
Sadriddin Tojiboev <sadriddin2309@gmail.com>
2022-04-26 17:01:28 +0500

    Blade: The Absolute Basics

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -8,11 +8,11 @@
 <body>
     <article>
        <h1>
-          <?= $post->title; ?>
+          {{ $post->title }}
        </h1>
 
        <div>
-          <?= $post->body; ?>
+          {!! $post->body !!}
        </div>
     </article>
 
